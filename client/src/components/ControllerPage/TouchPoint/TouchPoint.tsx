b2b2b66a36bc8c7823b92c29c8a3acff9539026b
Moses <mov7963@gmail.com>
2022-12-01 10:29:16 -0800

    working on phone slide movement

--- client/src/components/ControllerPage/TouchPoint/TouchPoint.tsx
+++ client/src/components/ControllerPage/TouchPoint/TouchPoint.tsx
@@ -1,5 +1,5 @@
 
-import { useCallback, useState, useRef, useEffect } from "react";
+import React, { useCallback, useState, useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../redux";
 import { DirectionChoices, PlayerChoices } from "../../../util/types";
@@ -18,18 +18,25 @@ const TouchPoint = ({ direction, Arrow }: Props) => {
   const { socket,sessionId, player } = useSelector(
     ({ socket: { socket }, menu:{sessionId,player} }: RootState) => ({ socket,sessionId,player })
   );
+  const holding = useRef<boolean>(false)
+  const normalize = (e:React.PointerEvent<HTMLButtonElement>) => {
+
+  }
   return (
     <button
     className="neonButton neonBorder neonText touchPoint"
       onTouchStart={(e) => {
+        holding.current = true
        //Tells the connected paddle to start moving in the given direction
         socket.emit("MOVE_PADDLE", sessionId, direction, player,true); 
       }}
       onTouchEnd={(e) => {
+        holding.current = false
       
        //Tells the connected paddle to stop moving
         socket.emit("MOVE_PADDLE", sessionId, direction, player,false);
       }}
+    
     >
       {Arrow}
     </button>
--- client/src/pages/ControllerPage/ControllerPage.tsx
+++ client/src/pages/ControllerPage/ControllerPage.tsx
@@ -1,12 +1,33 @@
 import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/ai";
 import TouchPoint from "../../components/ControllerPage/TouchPoint/TouchPoint";
-import './ControllerPage.css'
+import { useRef } from "react";
+import "./ControllerPage.css";
 const ControllerPage = () => {
+  const viewportHeight = useRef<number>(document?.body.clientHeight);
+  const handleMovement = (e: React.TouchEvent<HTMLDivElement>) => {
+    const touch = e.targetTouches.item(0);
+    const y = {
+      client: touch.clientY,
+      viewport: viewportHeight.current,
+    };
+    const centerClientY = y.client / viewportHeight.current - 0.5;
+  };
+
   return (
-    <div className="controllerPageContainer">
+    <div
+      className="controllerPageContainer"
+      // onPointerDown={(e) =>{
+      //   console.log('start')
+      // }}
+      // onPointerUp ={(e) => {
+      //   console.log('end')
+
+      // }}
+      onTouchMove={handleMovement}
+    >
       {" "}
-      <TouchPoint direction="up" Arrow={<AiOutlineArrowUp />} />
-      <TouchPoint direction="down" Arrow={<AiOutlineArrowDown />} />
+      {/* <TouchPoint direction="up" Arrow={<AiOutlineArrowUp />} />
+      <TouchPoint direction="down" Arrow={<AiOutlineArrowDown />} /> */}
     </div>
   );
 };
