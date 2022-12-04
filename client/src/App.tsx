9e7656296c620d4b30e076822b7cd29db55a41de
Moses <mov7963@gmail.com>
2022-12-04 12:06:55 -0800

    fixed screen sizing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,14 +14,7 @@ import "./App.css";
 
 function App() {
   useHostDC();
-  useEffect(() => {
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-  },[])
+
 
   return (
     <div className="App" 
